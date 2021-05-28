b6f7ba0725c0a8fe72939e36dd7ee7487c2fbd26
Spiritusanti <mccrackenjacob@gmail.com>
2021-05-28 10:54:38 -0700

    started adding state to project. Added user input form component under NewExpense folder

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.style.css";
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
