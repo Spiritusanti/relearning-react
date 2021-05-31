343df96842a8bcc25344f6f9411530569c053f88
Spiritusanti <mccrackenjacob@gmail.com>
2021-05-31 10:28:23 -0700

    added form data handling from input up to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import {useState} from 'react';
 import "./App.css";
 import Expenses from "./components/expenses/expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-import { expenses } from "./expense-data";
+// import { expenses } from "./expense-data";
 const App = () => {
+  const [expenses, setExpenses] = useState([])
+  const addExpensehandler = expense => {
+    setExpenses((prevState) => {
+     return [...prevState, expense]
+    })
+  };
+
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense onAddExpense={addExpensehandler}/>
       <Expenses expenses={expenses} />
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,97 @@
+import { useState } from "react";
 import "./ExpenseForm.style.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ onSaveExpenseData }) => {
+  /* using multiple state slices */
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+  /* using a single state object */
+  // const [userInput, setUserInput] = useState({
+  //   title: "",
+  //   amount: "",
+  //   date: "",
+  // });
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+    /* options for using a single state object */
+    // setUserInput({
+    //   ...userInput,
+    //   title: event.target.value,
+    // });
+    /* Use the following function syntax whenever your state depends on the prevState  */
+    // setUserInput((prevState) => {
+    //   return { ...prevState, title: event.target.value };
+    // });
+  };
+
+  const amountHandler = (event) => {
+    setAmount(event.target.value);
+    /* options for using a single state object */
+    // setUserInput({
+    //   ...userInput,
+    //   amount: event.target.value,
+    // });
+    /* Use the following function syntax whenever your state depends on the prevState  */
+    // setUserInput((prevState) => {
+    //   return { ...prevState, amount: event.target.value };
+    // });
+  };
+
+  const dateHandler = (event) => {
+    setDate(event.target.value);
+    /* options for using a single state object */
+    // setUserInput({
+    //   ...userInput,
+    //   date: event.target.value,
+    // });
+    /* Use the following function syntax whenever your state depends on the prevState  */
+    // setUserInput((prevState) => {
+    //   return { ...prevState, date: event.target.value };
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: title,
+      amount: amount,
+      date: new Date(date),
+    };
+
+    onSaveExpenseData(expenseData);
+    setTitle("");
+    setAmount("");
+    setDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" value={title} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={amount}
+            onChange={amountHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={date}
+            onChange={dateHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
