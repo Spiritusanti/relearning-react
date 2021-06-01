3995bdb445ff8240e478c71a975c91033ec7b89b
Spiritusanti <mccrackenjacob@gmail.com>
2021-05-31 17:28:22 -0700

    added starting files for ExpenseFilter and linked up to expense component

--- src/components/expenses/expenses.js
+++ src/components/expenses/expenses.js
@@ -1,22 +1,34 @@
-import "./expenses.style.css";
+import { useState } from "react";
 import ExpenseItem from "./expenseitem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/card";
+import "./expenses.style.css";
 
 const Expenses = (props) => {
   const { expenses } = props;
+  const [filterYear, setFilterYear] = useState("2021");
+
+  const filterHandler = (filteredYear) => {
+    setFilterYear(filteredYear);
+    console.log(filterYear);
+  };
+
   return (
-    <Card className="expenses">
-      {expenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            date={expense.date}
-            title={expense.title}
-            amount={expense.amount}
-          />
-        );
-      })}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filterYear} onFilterHandler={filterHandler} />
+        {expenses.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              date={expense.date}
+              title={expense.title}
+              amount={expense.amount}
+            />
+          );
+        })}
+      </Card>
+    </div>
   );
 };
 
